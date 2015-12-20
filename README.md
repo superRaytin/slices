5df82b80d922ec05c4882dbbacbbae00a141b2a6
leon.shi <superRaytin@163.com>
2015-12-21 00:11:10 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# slicy
-Converting image into slices by the given x and y axis of coordinate for the reference lines
+# slices
+
+Converting image into slices by the given reference lines
+
+```
+var Slices = require('slices');
+// Slices(width, height, lineXArray, lineYArray [, options]);
+var blocks = Slices(500, 500, [100], [100]);
+```
+
+Get blocks:
+
+```
+[
+    { width: 100, height: 100, x: 0, y: 0 },
+    { width: 400, height: 100, x: 100, y: 0 },
+    { width: 100, height: 400, x: 0, y: 100 },
+    { width: 400, height: 400, x: 100, y: 100 }
+]
+```
+
+# Installation
+
+```
+npm install slices
+```
+
+# Testing
+
+```
+npm test
+```
+
+# License
+
+MIT, see the [LICENSE](/LICENSE) file for detail.
+
---
+++ test/tests.js
@@ -0,0 +1,35 @@
+
+var should = require('should');
+var Slices = require('../lib/index');
+var utils = require('../lib/utils');
+
+
+var width = 500;
+var height = 500;
+var lineXArray = [100, 300];
+var lineYArray = [100, 300];
+
+
+describe('entrance method Slices()', function() {
+  it('Slices(width, height, lineXArray, lineYArray) works', function() {
+    var blocks = Slices(width, height, lineXArray, lineYArray);
+    var result = [{ width: 100, height: 100, x: 0, y: 0 },
+      { width: 200, height: 100, x: 100, y: 0 },
+      { width: 200, height: 100, x: 300, y: 0 },
+      { width: 100, height: 200, x: 0, y: 100 },
+      { width: 200, height: 200, x: 100, y: 100 },
+      { width: 200, height: 200, x: 300, y: 100 },
+      { width: 100, height: 200, x: 0, y: 300 },
+      { width: 200, height: 200, x: 100, y: 300 },
+      { width: 200, height: 200, x: 300, y: 300 }];
+    should(blocks).eql(result);
+    //console.log(Slices(width, height, lineXArray, lineYArray, {middleBoundaryMode: true}));
+
+    var blocks2 = Slices(width, height, [100], [100]);
+    var result2 = [{ width: 100, height: 100, x: 0, y: 0 },
+      { width: 400, height: 100, x: 100, y: 0 },
+      { width: 100, height: 400, x: 0, y: 100 },
+      { width: 400, height: 400, x: 100, y: 100 }];
+    should(blocks2).eql(result2);
+  });
+});
