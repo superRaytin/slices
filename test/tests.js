e13a55ca5cac53eaf4d5c738e9d4e60fdb714f92
leon.shi <superRaytin@163.com>
2015-12-22 18:54:32 +0800

    add test case

--- test/tests.js
+++ test/tests.js
@@ -3,12 +3,10 @@ var should = require('should');
 var Slices = require('../lib/index');
 var utils = require('../lib/utils');
 
-
 var width = 500;
 var height = 500;
 var lineXArray = [100, 300];
 var lineYArray = [100, 300];
-
 
 describe('entrance method Slices()', function() {
   it('Slices(width, height, lineXArray, lineYArray) works', function() {
@@ -31,5 +29,8 @@ describe('entrance method Slices()', function() {
       { width: 100, height: 400, x: 0, y: 100 },
       { width: 400, height: 400, x: 100, y: 100 }];
     should(blocks2).eql(result2);
+
+    var result3 = Slices(width, height, [100, 300], [100, 200, 300], {middleBoundaryMode: true});
+    console.log(JSON.stringify(result3));
   });
 });
